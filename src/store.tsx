7a80e363ccc4063c5e7f7e7b8d9ce3ba8e872b60
Kolya Ivankov <kolyaivankov@MacBook-Air-Kolya.local>
2024-04-08 14:59:46 +0300

    fix problem with store

--- src/store.tsx
+++ src/store.tsx
@@ -24,7 +24,7 @@ export const useTodoStore = create<TodoStore>((set) => ({
       tasks: [
         ...state.tasks,
         {
-          id: idCounter++,
+          id: String(idCounter++),
           title: taskTitle,
           isCompleted: false,
         },
@@ -33,13 +33,14 @@ export const useTodoStore = create<TodoStore>((set) => ({
   },
   deleteTaskById: (taskId) => {
     set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== taskId),
+      tasks: state.tasks.filter((task) => task.id !== taskId.toString()),
     }));
   },
+
   toggleTaskCompletedById: (taskId) => {
     set((state) => ({
       tasks: state.tasks.map((task) => {
-        if (task.id === taskId) {
+        if (task.id === taskId.toString()) {
           return {
             ...task,
             isCompleted: !task.isCompleted,
